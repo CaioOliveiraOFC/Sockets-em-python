5a18d2ea6d02e10aac8eb5660299ef8e8537fb3d
CaioOliveira <caio.oliveira2014@hotmail.com>
2021-04-26 22:13:00 -0300

    Update TCPClient.py

--- TCPClient.py
+++ TCPClient.py
@@ -1,22 +1,22 @@
 #!/usr/bin/env python3.9
 
-#Importando o módulo socket
+# Importando o módulo socket e o módulo time.
 from socket import *
 from time import sleep
 
-#Criando as variáveis que armazenam a porta e o IP do servidor
+# Criando as variáveis que armazenam a porta e o IP do servidor.
 serverName = '127.0.0.1'
 serverPort = 12000
 print('Variáveis serverName e serverPort criadas')
 sleep(5)
 
-#Criando o socket SOCK_STREAM é o protocolo TCP
+# Criando o socket; SOCK_STREAM é o protocolo TCP.
 print('Criando o socket...')
 sleep(5)
 clientSocket = socket(AF_INET, SOCK_STREAM)
 print('Socket criado')
 
-#Em TCP precisamos criar um handshake para começar a conexão, para isso usaremoso socket de como 'Boas vindas' iniciando a conexão com o atributo dele chamdo connect() e depois faremos o processo de envio dos dados, é como se tivesse um túnel entre eles.
+# '.connect()' é o método que se conecta com o servidor
 clientSocket.connect((serverName, serverPort))
 print('Conectando ao servidor...')
 sleep(5)
